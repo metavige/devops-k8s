db8094bab852620f387e3888b18774cd71629e82
metavige <metavige@gmail.com>
2021-08-24 16:13:39 +0800

    feat: add traefik header
    - add forwareder header middleware
    - add xss header middleware

--- init.sh
+++ init.sh
@@ -7,24 +7,20 @@ source .env
 TRAEFIK_CONF_DIR=${PWD}/base/traefik/config
 K3S_MANIFETSS_DIR=${PWD}/volumes/manifests
 
-if [ ! -f ${K3S_MANIFETSS_DIR}/traefik-secret.yaml ]; then
-  if [ ! -f ${TRAEFIK_CONF_DIR}/certs/_wildcard.k8s.internal-key.pem ]; then
-    cd ${TRAEFIK_CONF_DIR}/certs &&
-      mkcert '*.k8s.internal' &&
-      cd ${OLDPWD}
-  fi
-
-  kubectl create secret tls traefik-secret \
-    --cert=${TRAEFIK_CONF_DIR}/certs/_wildcard.k8s.internal.pem \
-    --key=${TRAEFIK_CONF_DIR}/certs/_wildcard.k8s.internal-key.pem \
-    -n kube-system --dry-run=client -o yaml >${K3S_MANIFETSS_DIR}/traefik-secret.yaml
+if [ ! -f ${TRAEFIK_CONF_DIR}/certs/_wildcard.k8s.internal-key.pem ]; then
+  cd ${TRAEFIK_CONF_DIR}/certs &&
+    mkcert '*.k8s.internal' &&
+    cd ${OLDPWD}
 fi
 
-if [ ! -f ${K3S_MANIFETSS_DIR}/traefik-configmap.yaml ]; then
-  kubectl create configmap traefik-config \
-    --from-file=ssl.yml=${TRAEFIK_CONF_DIR}/conf/ssl.yml \
-    -n kube-system --dry-run=client -o yaml >${K3S_MANIFETSS_DIR}/traefik-configmap.yaml
-fi
+kubectl create secret tls traefik-secret \
+  --cert=${TRAEFIK_CONF_DIR}/certs/_wildcard.k8s.internal.pem \
+  --key=${TRAEFIK_CONF_DIR}/certs/_wildcard.k8s.internal-key.pem \
+  -n kube-system --dry-run=client -o yaml >${K3S_MANIFETSS_DIR}/traefik-secret.yaml
+
+kubectl create configmap traefik-config \
+  --from-file=ssl.yml=${TRAEFIK_CONF_DIR}/conf/ssl.yml \
+  -n kube-system --dry-run=client -o yaml >${K3S_MANIFETSS_DIR}/traefik-configmap.yaml
 
 k3d cluster create ${CLUSTER_NAME} \
   -p 80:80@loadbalancer \
